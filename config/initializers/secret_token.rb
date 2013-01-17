7dd6d606de8dc061b54ab7d5dc3c599a6d755959
cadamson <cadamson@umd.edu>
2013-01-17 16:59:37 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Project2::Application.config.secret_token = 'd148e0cc14f861789c92d9ffa0d393ebf2ecea37a2d51e559c2b857a52b0927010aa54b51c2554d692ccc8ba4a32da03f40643df08b41554cbb9404489ee2093'
